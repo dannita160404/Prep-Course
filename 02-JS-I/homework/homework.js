100cce6b95fdb824bc85623978b26dc764ad19de
dannita160404 <sofiadryp@gmail.com>
2022-09-05 17:10:26 -0500

    comentario

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = hola como estas espiritu sANTO te amo ;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 27072020;
 
 // Crea una variable booleana:
 const nuevoBool = null;
